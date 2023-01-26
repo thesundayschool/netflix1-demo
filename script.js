34184c9d0c83f45076e434011820496551a2946b
Dinesh Kumar <dinesh78088@gmail.com>
2023-01-26 21:02:05 +0530

    trailer option modify

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
 
-    if($(this).scrollTop() > 100){
+    if ($(this).scrollTop() > 100) {
         $('.nav').addClass('nav_bg', 1000, "easeOutBounce")
-    }else{
+    } else {
         $('.nav').removeClass('nav_bg', 1000, "easeOutBounce")
     }
 
@@ -12,58 +12,58 @@ $(window).scroll(function(){
 /* movie data */
 let movies = [
     {
-        name : 'Milli',
-        image : 'images/milli.webp',
-        time : '1h 45m',
+        name: 'Milli',
+        image: 'images/milli.webp',
+        time: '1h 45m',
         trailer: 'https://youtu.be/RX9sn_BRxZs'
     },
     {
-        name : 'Money Heist',
-        image : 'images/money-heist.jpg',
-        time : '1h 20m',
+        name: 'Money Heist',
+        image: 'images/money-heist.jpg',
+        time: '1h 20m',
         trailer: 'https://youtu.be/_InqQJRqGW4'
     },
     {
-        name : 'Dictator',
-        image : 'images/dictator.webp',
-        time : '1h 20m',
+        name: 'Dictator',
+        image: 'images/dictator.webp',
+        time: '1h 20m',
         trailer: 'https://youtu.be/cYplvwBvGA4'
     },
     {
-        name : 'Secred Games',
-        image : 'images/secred-games.jpg',
-        time : '1h 20m',
+        name: 'Secred Games',
+        image: 'images/secred-games.jpg',
+        time: '1h 20m',
         trailer: 'https://youtu.be/AkUgf2jIPyI'
     },
     {
-        name : 'Friends',
-        image : 'images/friends.webp',
-        time : '1h 20m',
+        name: 'Friends',
+        image: 'images/friends.webp',
+        time: '1h 20m',
         trailer: 'https://youtu.be/LTpmw0Ac6Zs'
     },
     {
-        name : 'Fauda',
-        image : 'images/fauda.jpg',
-        time : '1h 20m',
+        name: 'Fauda',
+        image: 'images/fauda.jpg',
+        time: '1h 20m',
         trailer: 'https://youtu.be/k-tILBxRiQ4'
     },
 
     {
-        name : 'Chopsticks',
-        image : 'images/chopsticks.jpg',
-        time : '1h 20m',
+        name: 'Chopsticks',
+        image: 'images/chopsticks.jpg',
+        time: '1h 20m',
         trailer: 'https://youtu.be/dxbCuuc-jp4'
     },
     {
-        name : 'Marvik',
-        image : 'images/marvik.jpg',
-        time : '1h 20m',
+        name: 'Marvik',
+        image: 'images/marvik.jpg',
+        time: '1h 20m',
         trailer: 'https://youtu.be/giXco2jaZ_4'
     },
     {
-        name : 'Double XL',
-        image : 'images/double-xl.webp',
-        time : '1h 20m',
+        name: 'Double XL',
+        image: 'images/double-xl.webp',
+        time: '1h 20m',
         trailer: 'https://youtu.be/cQmQy-Wu72g'
     },
 ]
@@ -71,23 +71,24 @@ let movies = [
 // console.log(movies)
 
 
-movies.forEach(function(result){
+movies.forEach(function (result) {
     console.log(result)
 
     document.querySelector('.movie_list').innerHTML += `
         <div class="movie_card">
-            <div class="movie_img">
-                <img src="${result.image}"/>
-            </div>
-            <div> ${result.name} <span>${result.time}</span></div>
-            <a href="${result.trailer}" target="_blank">watch</a>
+            <a href="${result.trailer}" target="_blank">
+                <div class="movie_img">
+                    <img src="${result.image}"/>
+                </div>
+                <div> ${result.name} <span>${result.time}</span></div>
+            </a>
         </div>
     
-    ` 
+    `
 })
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.movie_list').slick({
         dots: true,
         infinite: true,
